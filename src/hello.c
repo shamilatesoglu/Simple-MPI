54917e30465ed395e44c7b696b4b73289bfd38fc
M. Şamil Ateşoğlu <m.samilatesoglu@gmail.com>
2021-04-25 20:53:29 +0300

    Add ack messages to hello process

--- src/hello.c
+++ src/hello.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <mpi.h>
 
 int main(int argc, char *argv[], char *environ[])
 {
-    printf("Process created. PID: %d, Rank: %s\n", getpid(), argv[1]);
-
     int npes, myrank, number;
     MPI_Init(&argc, &argv);
     MPI_Comm_size(&npes);
     MPI_Comm_rank(&myrank);
     if (myrank % 2 == 0)
     {
-        MPI_Recv(&number, 1, sizeof(int), (myrank + 1) % npes, 0);
-        MPI_Send(&number, 1, sizeof(int), (myrank + 1) % npes, 0);
+        int other = (myrank + 1) % npes;
+        MPI_Recv(&number, 1, sizeof(int), other, 0);
+        printf("From process %d to process %d data = %d, RECEIVED!\n", other, myrank, number);
+        MPI_Send(&number, 1, sizeof(int), other, 0);
     }
     else
     {
         number = myrank;
-        MPI_Send(&number, 1, sizeof(int), (myrank - 1 + npes) % npes, 0);
-        MPI_Recv(&number, 1, sizeof(int), (myrank - 1 + npes) % npes, 0);
+        int other = (myrank - 1 + npes) % npes;
+        MPI_Send(&number, 1, sizeof(int), other, 0);
+        MPI_Recv(&number, 1, sizeof(int), other, 0);
+        printf("From process %d to process %d data = %d, RECEIVED!\n", other, myrank, number);
     }
     MPI_Finalize();
 }
--- src/my_mpirun.c
+++ src/my_mpirun.c
@@ -3,35 +3,45 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/wait.h>
 
-void create_processes(char *executable_path, int n, char *environ[]);
+void
+create_processes(pid_t *processes, char *executable_path, int n, char *environ[]);
 
-int main(int argc, char *argv[], char *environ[])
+int
+main(int argc, char *argv[], char *environ[])
 {
     if (argc != 3)
     {
         printf("There must be 2 arguments in the following order: The number of instances to create, and an executable path.");
     }
 
-    int number_of_processes = atoi(argv[1]);
+    int number_of_processes = (int) strtol(argv[1], NULL, 10);
     char *executable_path = argv[2];
 
-    create_processes(executable_path, number_of_processes, environ);
+    pid_t processes[number_of_processes];
+    create_processes(processes, executable_path, number_of_processes, environ);
+
+    for (int i = 0; i < number_of_processes; i++)
+    {
+        waitpid(processes[i], NULL, 0);
+    }
 
     return 0;
 }
 
-void create_processes(char *executable_path, int n, char *environ[])
+void
+create_processes(pid_t *processes, char *executable_path, int n, char *environ[])
 {
-    int pid[n];
     for (int i = 0; i < n; i++)
     {
-        if ((pid[i] = fork()) == 0)
+        if ((processes[i] = fork()) == 0)
         {
-            char arg_rank[10];
-            sprintf(arg_rank, "%d", i);  
+            char arg_rank[10], arg_n[10];
+            sprintf(arg_rank, "%d", i);
+            sprintf(arg_n, "%d", n);
 
-            char* argv[] = {executable_path, arg_rank, NULL};
+            char *argv[] = {executable_path, arg_rank, arg_n, NULL};
 
             if (execve(executable_path, argv, environ) == -1)
             {
